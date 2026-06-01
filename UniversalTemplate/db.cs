da71803e5611780c421744f67e97c2065e3be002
Vadim <vadimlg888@gmail.com>
2026-06-02 00:51:24 +0300

    Fix: point db connection string to exam_v5

--- UniversalTemplate/db.cs
+++ UniversalTemplate/db.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,7 @@ namespace UniversalApp
         // ВНИМАНИЕ НА ЭКЗАМЕНЕ:
         // Измените 'shoe2026_pu' на название базы данных из вашего варианта!
         // =========================================================================
-        public const string ConnectionString = "Server=127.0.0.1;Port=3306;Database=shoe2026_pu;Uid=root;Pwd=;Charset=utf8mb4;Allow Zero Datetime=True;Convert Zero Datetime=True;";
+        public const string ConnectionString = "Server=127.0.0.1;Port=3306;Database=exam_v5;Uid=root;Pwd=;Charset=utf8mb4;Allow Zero Datetime=True;Convert Zero Datetime=True;";
 
         public static MySqlConnection GetConnection()
         {
