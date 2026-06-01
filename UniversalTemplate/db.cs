c5c953a13389597f7be2c492eb8bebf6d36e8678
Vadim <vadimlg888@gmail.com>
2026-06-02 01:12:58 +0300

    Clean up comments in db.cs

--- UniversalTemplate/db.cs
+++ UniversalTemplate/db.cs
@@ -8,10 +8,7 @@ namespace UniversalApp
 {
     internal static class Db
     {
-        // =========================================================================
-        // ВНИМАНИЕ НА ЭКЗАМЕНЕ:
-        // Измените 'shoe2026_pu' на название базы данных из вашего варианта!
-        // =========================================================================
+        // Строка подключения к базе данных MySQL
         public const string ConnectionString = "Server=127.0.0.1;Port=3306;Database=exam_v5;Uid=root;Pwd=;Charset=utf8mb4;Allow Zero Datetime=True;Convert Zero Datetime=True;";
 
         public static MySqlConnection GetConnection()
